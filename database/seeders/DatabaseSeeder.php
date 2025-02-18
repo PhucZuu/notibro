5562b4131b30c85a4370f5ad12fdc90f5b753a76
honggiang <honggiangtran2004@gmail.com>
2025-02-18 22:08:03 +0700

    Cập nhật DatabaseSeeder và composer.lock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(RoleSeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(User_RoleSeeder::class);
-        // $this->call(ColorSeeder::class);
-        // $this->call(TimezoneSeeder::class);
-        // $this->call(TaskSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(User_RoleSeeder::class);
+        $this->call(ColorSeeder::class);
+        $this->call(TimezoneSeeder::class);
+        $this->call(TaskSeeder::class);
     }
 }
