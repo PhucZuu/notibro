b649e3b86fd9c7dfa2906ce35867a0a0bd7c8d0f
shinji2004 <tunaph44948@fpt.edu.vn>
2025-03-19 21:57:14 +0700

    [update] fix invite tags

--- routes/api.php
+++ routes/api.php
@@ -57,9 +57,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/event/{uuid}/refuse', [TaskController::class,'refuseInvite']);
     Route::get('/event/{uuid}/invite', [TaskController::class,'show']);
 
-    Route::post('/events/{id}/accept', [TagController::class, 'acceptTagInvite']);
-    Route::post('/events/{id}/refuse', [TagController::class, 'declineTagInvite']);
-    Route::get('/events/{id}/invite', [TagController::class, 'show']);
+    Route::post('/tags/{id}/accept', [TagController::class, 'acceptTagInvite']);
+    Route::post('/tags/{id}/refuse', [TagController::class, 'declineTagInvite']);
+    Route::get('/tags/{id}/invite', [TagController::class, 'show']);
 
     // Notifications
     Route::get('/notifications', [NotificationController::class, 'index']);
