6345237da9d0463385e71ff6c75d7105a87cdbc3
linhdaoisreal <uckgudu@gmail.com>
2025-03-21 16:02:40 +0700

    [update] Better Auto create task by AI

--- app/Http/Controllers/Api/OpenAI/OpenAIController.php
+++ app/Http/Controllers/Api/OpenAI/OpenAIController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api\OpenAI;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use App\Models\Task;
 use App\Services\OpenAIService;
 use App\Services\TaskSupportService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class OpenAIController extends Controller
@@ -28,7 +30,7 @@ class OpenAIController extends Controller
 
         // Lấy cấu trúc bảng từ model Task
         $tableStructure = Task::getTableStructure();
-        Log::info("Cấu trúc bảng tiền xử lý: ". json_encode($tableStructure));
+        Log::info("Cấu trúc bảng tiền xử lý: " . json_encode($tableStructure));
 
         // Gửi đến AI để phân tích
         $fields = $this->openAIService->analyzeRequest($userRequest, $tableStructure);
@@ -36,18 +38,21 @@ class OpenAIController extends Controller
         try {
             $created_result = $this->taskSupportService->store($fields);
 
-        return response()->json([
-            'code'    => 200,
-            'message' => 'OpenAI have created your task successfully',
-            'data'    => $created_result,
-        ], 200);
+            Log::info('Tạo thành công');
+            Log::info($created_result);
+
+            return response()->json([
+                'code'    => 200,
+                'message' => 'OpenAI have created your task successfully',
+                'data'    => $created_result,
+            ], 200);
         } catch (Exception $e) {
+            Log::info('Tạo thất bại');
             return response()->json([
                 'code'    => 500,
                 'message' => 'Failed to create task',
                 'error'   => $e->getMessage(),
             ], 500);
         }
-        
     }
 }
